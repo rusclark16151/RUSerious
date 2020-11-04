b585a89ad92dfde0344309c2e04a9852ffe7523e
madelyn007 <mcilwainmadelyn@gmail.com>
2020-11-03 20:15:01 -0600

    11/1/20

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneStickStrafe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneStickStrafe.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "oneStickStraff", group = "Training")
+@TeleOp(name = "Teleop1", group = "Training")
 public class OneStickStrafe extends OpMode {
 
     DcMotor topLeft;
@@ -17,14 +17,18 @@ public class OneStickStrafe extends OpMode {
     DcMotor backLeftMotor;
     DcMotor frontLeftMotor;
     DcMotor frontRightMotor;
+    DcMotor Shooter;
+    //DcMotor Intake;
 
     double rightStraffPower;
     double leftStraffPower;
     double leftWheelPower;
     double rightWheelPower;
+    boolean shooter50;
+    boolean shooter100;
+    boolean shooterOff;
     boolean intakeOn;
     boolean intakeOff;
-    boolean intakeRev;
     boolean foundationUp;
     boolean foundationDown;
     boolean straffOn;
@@ -36,12 +40,15 @@ public class OneStickStrafe extends OpMode {
     double x;
     double rx;
     double y;
+    boolean shooter;
+    int count = 0;
+    boolean hascount = false;
 
-    CRServo lIntake;
+   /*CRServo lIntake;
     CRServo rIntake;
 
     Servo lFoundation;
-    Servo rFoundation;
+    Servo rFoundation;*/
 
 
     @Override
@@ -54,12 +61,14 @@ public class OneStickStrafe extends OpMode {
         frontRightMotor = hardwareMap.dcMotor.get("topRight");
         backRightMotor = hardwareMap.dcMotor.get("bottomRight");
         backLeftMotor = hardwareMap.dcMotor.get("bottomLeft");
+        Shooter = hardwareMap.dcMotor.get("shooter");
+        // Intake = hardwareMap.dcMotor.get("intake"); //this is a place holder for intake
 
-        rIntake = hardwareMap.crservo.get("Right Intake");
-        lIntake = hardwareMap.crservo.get("left intake servo");
+        // rIntake = hardwareMap.crservo.get("Right Intake");
+        //lIntake = hardwareMap.crservo.get("left intake servo");
 
-        lFoundation = hardwareMap.servo.get("Left Foundation");
-        rFoundation = hardwareMap.servo.get("Right Foundation");
+        //  lFoundation = hardwareMap.servo.get("Left Foundation");
+        //   rFoundation = hardwareMap.servo.get("Right Foundation");
 
 
         //topRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -72,10 +81,10 @@ public class OneStickStrafe extends OpMode {
         y = gamepad1.left_stick_y;
         x = gamepad1.left_stick_x;
         rx = gamepad1.right_stick_x;
-        frontLeftMotor.setPower(-1 * (y + x + rx));
-        backLeftMotor.setPower(-1 * (y - x + rx));
-        frontRightMotor.setPower(y - x - rx);
-        backRightMotor.setPower(y + x - rx);
+        frontLeftMotor.setPower(-0.25 * (y - x + rx));
+        backLeftMotor.setPower(0.25 * (y - x + rx));
+        frontRightMotor.setPower(-0.25 * (y + x - rx));
+        backRightMotor.setPower(0.25 * (y + x - rx));
 
 
        /* leftWheelPower = -gamepad1.left_stick_y;
@@ -86,65 +95,94 @@ public class OneStickStrafe extends OpMode {
 
         rightBmp = gamepad1.right_bumper;
         leftBmp = gamepad1.left_bumper;
-        intakeOn = gamepad1.b;
-        intakeOff = gamepad1.x;
-        intakeRev = gamepad1.a;
+        shooterOff = gamepad1.dpad_down;
+        shooter50 = gamepad1.dpad_left;
+        shooter100 = gamepad1.dpad_up;
+        // intakeRev = gamepad1.a;
         foundationDown = gamepad2.left_bumper;
         foundationUp = gamepad2.right_bumper;
 
-        /*if (rightBmp && straffOn == false) {
+        if (gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.dpad_right) {
+            if (gamepad1.dpad_up) {
+                if (hascount == false) {
+                    if(count < 3) {
+                        hascount = true;
+                        count = count + 1;
+                    }
+                }
+            }
+            if (gamepad1.dpad_down) {
+                if (hascount == false) {
+                    if (count > 0) {
+                        hascount = true;
+                        count = count - 1;
+                    }
+                }
+            }
+            if (gamepad1.dpad_right) {
+                if (hascount == false) {
+                    hascount = true;
+                    count = 3;
+                }
+            }
+            if (gamepad1.dpad_left) {
+                if (hascount == false) {
+                    hascount = true;
+                    count = 4;
+                }
+            }
 
-            straffOn = true;
+            }
+        if (!gamepad1.dpad_up && !gamepad1.dpad_down && !gamepad1.dpad_left && !gamepad1.dpad_right) {
+            hascount = false;
+            if (count == 4){
+                count = 0;
+            }
         }
-        if (straffOn == true) {
-            backRightMotor
-.setPower(rightStraffPower);
-            bottomLeft.setPower(leftStraffPower);
-            topRight.setPower(-rightStraffPower);
-            topLeft.setPower(-leftStraffPower);
+         switch (count) {
+                case 1:
+                        Shooter.setPower(0.25);
+                case 2:
+                        Shooter.setPower(0.5);
+                case 3:
+                        Shooter.setPower(1);
+                case 4:
+                        Shooter.setPower(0);
+            }
+
+        telemetry.addData("Count", count);
+        telemetry.addData("hasCount",hascount);
+        telemetry.update();
+    }
+
+}
+
+
+        /*if (shooter100) {
+
+
         }
-        if (leftBmp && straffOn == true)
-        {
-            straffOn = false;
-        }
-
-        if (straffOn == false)
-        {
-            backRightMotor
-.setPower(rightWheelPower);
-            bottomLeft.setPower(leftWheelPower);
-            topLeft.setPower(leftWheelPower);
-            topRight.setPower(rightWheelPower);
-        }
+        if (shooterOff) {
+            Shooter.setPower(0);
 
 
-        if (intakeOn) {
-            lIntake.setPower(1);
-            rIntake.setPower(-1);
-            //rIntake.setPosition(1);
 
+        if (shooter50){
+            Shooter.setPower(.5);*/
+
+       /*This is a place holder for when we add the intake motor
+
+       if (intakeOn) {
+            Intake.setPower(1);
 
         }
         if (intakeOff) {
-            lIntake.setPower(0);
-            rIntake.setPower(0);
-
-        }
-        if (intakeRev) {
-            lIntake.setPower(-1);
-            rIntake.setPower(1);
-        }
-        if (foundationUp) {
-            lFoundation.setPosition(.5);
-            rFoundation.setPosition(.6);
+            Intake.setPower(0);*/
 
 
-        }
 
-        if (foundationDown) {
-            rFoundation.setPosition(0);
-            lFoundation.setPosition(1);
 
-        }*/
-    }
-}
+
+
+
+

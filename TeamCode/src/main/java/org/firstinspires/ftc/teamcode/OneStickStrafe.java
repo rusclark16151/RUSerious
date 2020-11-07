aed6131722ca6bb1d8308a142a0b9db6b04888b0
madelyn007 <mcilwainmadelyn@gmail.com>
2020-11-07 15:14:55 -0600

    11/7/20

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneStickStrafe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneStickStrafe.java
@@ -3,81 +3,45 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Teleop1", group = "Training")
 public class OneStickStrafe extends OpMode {
 
-    DcMotor topLeft;
-    DcMotor topRight;
-    DcMotor bottomLeft;
     DcMotor backRightMotor;
     DcMotor backLeftMotor;
     DcMotor frontLeftMotor;
     DcMotor frontRightMotor;
     DcMotor Shooter;
     DcMotor Intake;
+    Servo hammerServo;
 
-    double rightStraffPower;
-    double leftStraffPower;
-    double leftWheelPower;
-    double rightWheelPower;
-    boolean shooter50;
-    boolean shooter100;
-    boolean shooterOff;
     boolean isIntakeOn;
     boolean IntakeON;
-    //boolean intakeOff;
-    boolean foundationUp;
-    boolean foundationDown;
-    boolean straffOn;
-    boolean rightBmp;
-    boolean leftBmp;
-    double lsX;
-    double lsY;
-    double rsX;
+    float trigger;
     double x;
     double rx;
     double y;
-    boolean shooter;
     int count = 0;
-    int count2 = 0;
     boolean hascount = false;
     boolean hasToggled = false;
-
-   /*CRServo lIntake;
-    CRServo rIntake;
-
-    Servo lFoundation;
-    Servo rFoundation;*/
-
+    double servoMax = 0.5;
+    double servoMin = 0.0;
 
     @Override
     public void init() {
-        // topLeft = hardwareMap.dcMotor.get("topLeft");
-        // topRight = hardwareMap.dcMotor.get("topRight");
-        // bottomRight = hardwareMap.dcMotor.get("bottomRight");
-        //bottomLeft = hardwareMap.dcMotor.get("bottomLeft");
+        //this is the set up for the hardware configuration during init
         frontLeftMotor = hardwareMap.dcMotor.get("topLeft");
         frontRightMotor = hardwareMap.dcMotor.get("topRight");
         backRightMotor = hardwareMap.dcMotor.get("bottomRight");
         backLeftMotor = hardwareMap.dcMotor.get("bottomLeft");
         Shooter = hardwareMap.dcMotor.get("shooter");
-        Intake = hardwareMap.dcMotor.get("intake"); //this is a place holder for intake
-
-        // rIntake = hardwareMap.crservo.get("Right Intake");
-        //lIntake = hardwareMap.crservo.get("left intake servo");
-
-        //  lFoundation = hardwareMap.servo.get("Left Foundation");
-        //   rFoundation = hardwareMap.servo.get("Right Foundation");
-
-
-        //topRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        //bottomRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        Intake = hardwareMap.dcMotor.get("intake");
+        hammerServo = hardwareMap.servo.get("hammer");
 
     }
 
+    //this is the main loop for the teleOP program when play is pressed
     @Override
     public void loop() {
         y = gamepad1.left_stick_y;
@@ -88,21 +52,7 @@ public class OneStickStrafe extends OpMode {
         frontRightMotor.setPower(-0.25 * (y + x - rx));
         backRightMotor.setPower(0.25 * (y + x - rx));
 
-
-       /* leftWheelPower = -gamepad1.left_stick_y;
-        rightWheelPower = -gamepad1.right_stick_y;
-        leftStraffPower = gamepad1.left_stick_x;
-       rightStraffPower = -gamepad1.right_stick_x;
-*/
-
-        rightBmp = gamepad1.right_bumper;
-        leftBmp = gamepad1.left_bumper;
-        shooterOff = gamepad1.dpad_down;
-        shooter50 = gamepad1.dpad_left;
-        shooter100 = gamepad1.dpad_up;
-        // intakeRev = gamepad1.a;
-        foundationDown = gamepad2.left_bumper;
-        foundationUp = gamepad2.right_bumper;
+        trigger = gamepad1.right_trigger;
         IntakeON = gamepad1.a;
 
 
@@ -137,7 +87,8 @@ public class OneStickStrafe extends OpMode {
             }
 
         }
-        if (!gamepad1.dpad_up && !gamepad1.dpad_down && !gamepad1.dpad_left && !gamepad1.dpad_right) {
+        if (!gamepad1.dpad_up && !gamepad1.dpad_down &&
+                !gamepad1.dpad_left && !gamepad1.dpad_right) {
             hascount = false;
             if (count == 4) {
                 count = 0;
@@ -154,12 +105,12 @@ public class OneStickStrafe extends OpMode {
                 Shooter.setPower(0);
         }
 
-        telemetry.addData("toggle", hasToggled);
+        telemetry.addData("servoPosition", hammerServo.getPosition());
         telemetry.addData("hasCount", hascount);
         telemetry.addData("isIntakeon", isIntakeOn);
         telemetry.update();
 
-        //This is a place holder for when we add the intake motor
+        //This code turns the intake ON and OFF
 
         if (IntakeON && isIntakeOn == false && hasToggled == false) {
             isIntakeOn = true;
@@ -175,8 +126,17 @@ public class OneStickStrafe extends OpMode {
         if (IntakeON == false && isIntakeOn == false) {
             hasToggled = false;
         }
+
+        //this code is used for the trigger
+        if (trigger > 0) {
+            hammerServo.setPosition(servoMax);
+        }
+        else {
+            hammerServo.setPosition(servoMin);
+        }
     }
 }
+
 
 
 
